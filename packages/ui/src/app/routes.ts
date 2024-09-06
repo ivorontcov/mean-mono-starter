089ed80faade2fba14ea71fb22e787dc6e1ce420
ivorontcov <82820168+ivorontcov@users.noreply.github.com>
2024-09-06 14:16:29 -0400

    allow react app being integrated

--- packages/ui/src/app/routes.ts
+++ packages/ui/src/app/routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+// import { loadRemoteModule } from '@angular-architects/native-federation';
+import { WebComponentWrapper, WebComponentWrapperOptions } from '@angular-architects/module-federation-tools';
 import { HomeComponent } from './components/home/home.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { checkAuth } from './guards/check-auth';
@@ -14,6 +16,22 @@ export const APP_ROUTES: Routes = [
         path: 'workflows',
         component: HomeComponent,
         canActivate: [checkAuth],
+    },
+    // {
+    //     path: 'visualizer',
+    //     loadComponent: () =>
+    //       loadRemoteModule('mfe1', './web-components').then((m) => m.AppComponent),
+    //   },
+    {
+        path: 'visualizer',
+        component: WebComponentWrapper,
+        data: {
+            type: 'script',
+            remoteEntry: 'http://localhost:4204/remoteEntry.js',
+            remoteName: 'reactChild',
+            exposedModule: './web-components',
+            elementName: 'child-react-element',
+        } as WebComponentWrapperOptions,
     },
     {
         path: 'landing',
