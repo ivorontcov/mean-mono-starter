eee79d3db82a02b7092d9ae552c9c87e88fe48eb
ivorontcov <82820168+ivorontcov@users.noreply.github.com>
2024-09-06 14:16:42 -0400

    ammend last commit

--- packages/ui/webpack.config.js
+++ packages/ui/webpack.config.js
@@ -1,13 +1,11 @@
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
+    remotes: {
+        mfe1: 'http://localhost:3000/remoteEntry.js',
+    },
 
-  remotes: {
-    "mfe1": "http://localhost:3000/remoteEntry.js",    
-  },
-
-  shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  },
-
+    shared: {
+        ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    },
 });
